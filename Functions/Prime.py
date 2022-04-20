5a4bbf7e35ffcc341c10eab6626dc8ac835654c2
KPRoshini <102510968+KPRoshini@users.noreply.github.com>
2022-04-20 13:13:54 +0530

    Update Prime.py

--- Functions/Prime.py
+++ Functions/Prime.py
@@ -1,10 +1,13 @@
-n=int(input("Enter a number:"))
-def prime(n):
-  for i in range(2,n):
-    if (n % i ==0):
-      print("It is not a Prime number")
-      break
-    else:
-      print("It is a Prime number")
-      break
-prime(n)
+def prime_checker(n):
+  c=0
+  if n!=1:
+    for i in range (1,n+1):
+      if(n%i==0):
+        c=c+1
+  if(c==2):
+    print("it is a prime number")
+  else:
+    print("it is not a prime number")
+
+n=int(input("enter a number : "))
+prime_checker(n)
