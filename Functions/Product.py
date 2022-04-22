6dce6c3927907af9011500074c04af91226f15e3
KPRoshini <102510968+KPRoshini@users.noreply.github.com>
2022-04-22 16:37:12 +0530

    Update Product.py

--- Functions/Product.py
+++ Functions/Product.py
@@ -1,8 +1,18 @@
-a=int(input("Enter the first number:"))
-b=int(input("Enter the second number:"))
-def mul(n,m):
-  if(a*b <=500):
-      return a*b
+def prod_or_sum(n1,n2):
+  s=0
+  rem=0
+  pro=n1*n2
+  if(pro > 500):
+    print("product of ",n1," and ",n2," is greater than 500 ")
+    print("Their product is : ",pro)
+    while(pro>0):
+      rem = pro % 10
+      s=s+rem
+      pro=pro//10
+    print("So, sum of their product is: ",s)
   else:
-      return a+b
-print(mul(a,b))
+    print("product is : ",pro)
+
+num1=int(input("Enter number1: "))
+num2=int(input("Enter number2: "))
+prod_or_sum(num1,num2)
